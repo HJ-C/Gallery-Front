17a01d19a843dcd14bbb31404d7bb2b0c4479aa1
HJ-C <swch56@naver.com>
2022-09-06 14:38:40 +0900

    #comment

--- src/components/Main/Content.jsx
+++ src/components/Main/Content.jsx
@@ -4,8 +4,48 @@ import UpLoad from '../PostReg/upload';
 import { Link } from "react-router-dom";
 import postCommentInFeed from './comment';
 
-function Content({pfUser,setPfUser,mainImg,i}){
+function Content({pfUser,mainImg,i}){
+  const [comment, setComment] = useState('');
+  const [commentArray, setCommentArray] = useState([]);
+
+  const onSubmit = e => {
+    e.preventDefault();
+    if (comment === '') {
+      return;
+    }
+    setCommentArray(a => [comment, ...a]);
+    setComment('');
+  };
+
+  const onChange = event => {
+    setComment(event.target.value);
+  }
+  // useEffect(()=>{
+  //   axios.get('https://jsonplaceholder.typicode.com/comments?postId=1')
+  //     .then((res)=>{
+  //       console.log(res.data)
+  //       setComment([...res.data])
+  //     })
+  // },[])
   
+// 댓글
+// const onCommentHandler = (e) =>{
+//   setComment(e.currentTarget.value)
+// }
+    
+// const commentSubmit = () => {
+//   axios.post('',{
+//     comment : comment
+//   },{
+//     headers: {
+//       'Content-Type': 'application/json'
+//       }
+//   })
+//   .then(result => console.log('결과: ', result));
+// }
+
+
+
 
     // id=1일 데이터 받아오기
     // const Pid = props.mainImg.filter((a,i) =>{
@@ -48,29 +88,38 @@ function Content({pfUser,setPfUser,mainImg,i}){
           <img src="assets/Main/more_btn.png" alt="" />
         </button>
       </div>
-      <div className="post__infos">
+      <div className="post__infos" onSubmit={onSubmit}>
         <div className="post__title">
           <span>{mainImg[i].title}</span>
         </div>
         <div className="post__description">
           <span>{mainImg[i].title}</span>
         </div>
+
         <div className="post__border">
         </div>
         {/*
         <!-- 댓글 --> */}
-        <div>
-          <ul className="comment_list">
-            <li>
-            </li>
-          </ul>
-        </div>
-        <section className="post_comment_wrap">
+     <div>
+      <ul className="comment_list">
+      {commentArray.map((value, id) => (
+            <li key={id}>
+              <div class="user_desc">
+                <em>iAmUser</em>
+                <span>{value}</span>
+              </div>
+           </li>
+         ))} 
+      </ul>
+    </div> 
+      <form>
+        <section className="post_comment_wrap" >
           <input 
             id="post_comment_input" 
             type="text" 
             placeholder="댓글 달기..." 
-            // onChange={onCommentHandler}
+            value={comment}
+            onChange={onChange}
             />
           <button 
             className="post_comment_btn"
@@ -79,6 +128,7 @@ function Content({pfUser,setPfUser,mainImg,i}){
             <i className='bx bx-send' ></i>
           </button>
         </section>
+        </form>
         <span className="post__date-time">more</span>
       </div>
     </div>
